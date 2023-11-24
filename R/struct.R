1e815f31d5969cafd3529013c496c58bfdc2902a
joakim219 <jantmjyv@student.jyu.fi>
2023-11-24 13:11:50 +0200

    S3 method for dynamitefit

--- R/struct.R
+++ R/struct.R
@@ -1,27 +1,6 @@
 # "struct" is a temporary name for the structure that holds the necessary
 # data to plot the transition probabilities
 
-# Idea:
-#
-# Calling
-#
-# struct(
-#   fit,
-#   x = list(name = "age", values = 0:100),
-#   group = list(name = "sex", values = c("male", "female")),
-#     or
-#   fixed_predictors = list(sex = "male"))
-#
-# returns
-#
-# list(
-#   x = list(name = "age", values = 0:100),
-#   group = list(name = "sex", values = c("male", "female")),
-#   prob = matrix(ncol = ngroup * nstate * nstate),
-#   l_prob = matrix(ncol = ngroup * nstate * nstate),
-#   u_prob = matrix(ncol = ngroup * nstate * nstate)
-# )
-
 format_transition <- Vectorize(function(group, from, to) {
   if (is.null(group)) {
     return(paste0(from, "->", to))
@@ -37,15 +16,15 @@ prob_template <- function(states, x, group) {
   if (is.null(group)) {
     return(setnames(
       matrix(nrow = nx, ncol = nstates * nstates) |>
-        as.data.table(),
-      expand.grid(states, states) |>
+        data.table::as.data.table(),
+      expand.grid(states, states) |> # TODO CJ?
         apply(MARGIN = 1, FUN = \(row) format_transition(NULL, row[2], row[1]))
     ))
   } else {
     ngroups <- length(group$values)
     return(setnames(
       matrix(nrow = nx, ncol = nstates * nstates * ngroups) |>
-        as.data.table(),
+        data.table::as.data.table(),
       expand.grid(group$values, states, states) |>
         apply(MARGIN = 1, FUN = \(row) format_transition(row[1], row[3], row[2]))
     ))
@@ -56,18 +35,14 @@ prob_template <- function(states, x, group) {
 struct <- function(...) UseMethod("struct")
 
 #' @export
-struct.manual <- function(state, x, group = NULL, prob, l_prob = NULL, u_prob = NULL) {
+struct.manual <- function(state, x, group = NULL, prob) {
   n <- length(x)
   s <- list(state = state, x = x, group = group, prob = prob)
   class(s) <- "struct"
-  s[["l_prob"]] <- l_prob
-  s[["u_prob"]] <- u_prob
   return(s)
 }
 
-#' @importFrom data.table as.data.table
-#' @importFrom data.table data.table
-#' @importFrom data.table `:=`
+#' @import data.table
 #' @export
 struct.multinom <- function(fit,
                             state_name,
@@ -95,8 +70,9 @@ struct.multinom <- function(fit,
 
   try(new_data[, names(fixed_predictors) := NULL], silent = TRUE)
 
-  prob <- new_data[rep(1:nrow(new_data), each = ncol(p))]
-  prob[, paste0("trans_", c("dest", "prob")) :=
+  prob <- new_data[rep(1:.N, each = ncol(p))]
+  colnames(prob) <- c("x", "from", "group")
+  prob[, c("to", "mean") :=
          list(rep(colnames(p), times = nrow(new_data)), c(t(p)))]
 
   return(struct.manual(
@@ -108,9 +84,77 @@ struct.multinom <- function(fit,
 }
 
 #' @export
-struct.dynamitefit <- function(fit) {
+struct.dynamitefit <- function(fit,
+                               state_name,
+                               x_values = NULL,
+                               group = NULL,
+                               fixed_predictors = NULL,
+                               interval = NULL) {
+  state <- list(name = state_name, values = levels(fit$data[[state_name]]))
+  x <- list(name = fit$time_var, values = orElse(x_values, unique(fit$data[[fit$time_var]])))
+
+  n_states <- length(state$values)
+
+  prob <- data.table::CJ(
+    x = x$values[-1],
+    group = orElse(group$values, NA),
+    from = state$values,
+    to = state$values,
+    mean = 0, lower = 0, upper = 0
+  )
+
+  if (is.null(group)) {
+    prob[, group := NULL]
+  }
+
+  has_random = any(grepl("(\\s|~)+random\\s*\\(", fit$call))
+  new_data <- unique(na.omit(fit$data)[, c(fit$group_var, group$name), with = FALSE])
+  if (!has_random) {
+    if (!is.null(group)) {
+      new_data <- unique(new_data, by = group$name)
+    } else {
+      new_data <- new_data[1]
+    }
+  }
+
+  n_new_data <- nrow(new_data)
+  new_data <- new_data[rep(1:.N, each = length(x$values))]
+  new_data <- new_data[, x$name := rep(x$values, n_new_data)]
+
+  try(new_data[, names(fixed_predictors) := fixed_predictors], silent = TRUE)
+
+  for (s in state$values) {
+    new_data[, state$name := list(s)]
+
+    p <- na.omit(fitted(fit, newdata = new_data, df = FALSE))[
+      ,
+      as.list(unlist(lapply(.SD, \(q) list(
+        mean = mean(q),
+        quantile = quantile(q, c(0.025, 0.975), names = FALSE)
+      )))),
+      .SDcols = 1:n_states,
+      by = c(x$name, group$name)
+    ]
+
+    setnames(p, c(x$name, orElse(group$name, "")), c("x", "group"), skip_absent = TRUE)
+
+    p <- melt(p,
+              id = NULL,
+              measure.vars = patterns(".+\\.mean$", ".+\\.quantile1$", ".+\\.quantile2$"),
+              value.name = c("mean", "lower", "upper"),
+              variable.name = "to")[, to := state$values[to]]
+    p[, from := list(s)]
+    prob[
+      p,
+      c("mean", "lower", "upper") := list(i.mean, i.lower, i.upper),
+      on = na.omit(c("x", ifelse(!is.null(group), "group", NA), "from", "to"))
+    ]
+  }
+
   return(struct.manual(
-    time = c(1, 2),
-    prob = c(1, 1)
+    state = state,
+    x = x,
+    group = group,
+    prob = prob
   ))
 }
