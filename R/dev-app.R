674220abea286a85f4fd34bf8727b0c30d78e0b8
joakim219 <jantmjyv@student.jyu.fi>
2023-11-24 13:14:01 +0200

    Fit a dynamitemodel

--- R/dev-app.R
+++ R/dev-app.R
@@ -1,22 +1,54 @@
 if (FALSE) {
 
+  #################
+  # nnet::multinom
+  #################
   ffit <- nnet::multinom(
     state ~ age + lagstate + sex,
-    data = gradu::health)
+    data = gradu::health
+  )
 
   ffit_struct <- gradu::struct(
     ffit,
     state_name = "lagstate",
-    x = list(name = "age", values = 0:100),
-    #fixed_predictors = list(sex = "male"))
-    group = list(name = "sex", values = c("male", "female")))
-
-  str(ffit_struct)
+    x = list(name = "age", values = 3:59),
+    #fixed_predictors = list(sex = "male")
+    group = list(name = "sex", values = c("male", "female"))
+  )
 
   plot(ffit_struct)
 
-  View(ffit_struct$prob)
+  #################
+  # dynamite
+  #################
+  bfit <- dynamite::dynamite(
+    dynamite::obs(state ~ varying(~ -1 + lag(state)) + sex, "categorical") +
+      dynamite::splines(df = 10),
+    data = gradu::health,
+    time = "age",
+    group = "id",
+    backend = "cmdstanr",
+    chains = 1,
+    refresh = 0,
+    thin = 5,
+    seed = 1
+  )
 
+  save(bfit, file = "bfit.RData")
+  load(file = "bfit.RData")
+
+  bfit_struct <- gradu::struct(
+    bfit,
+    state_name = "state",
+    #fixed_predictors = list(sex = "male")
+    group = list(name = "sex", values = c("male", "female"))
+  )
+
+  plot(bfit_struct)
+
+  #################
+  # effects::Effect
+  #################
   ffit_eff <- effects::Effect(
     c("age", "lagstate", "sex"),
     ffit,
